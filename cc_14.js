e7f4ac60e3a7bdc2094bece97b78b3138c639e52
Kamali Hicks <kamalikh3@gmail.com>
2025-03-09 19:28:20 -0400

    Task 5 - Inline Editing for Support Tickets

--- cc_14.js
+++ cc_14.js
@@ -20,27 +20,83 @@ function createSupportTicket(name, issue, priority) {
     customerCard.appendChild(customerPriority);
 
     // Task 3 Highlight High priority tickets
-
     if (priority === 'High') {
         customerCard.classList.add('high-priority')
     }
-
     
-    // Task 4: resolve button 
+    // Creating remove button 
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Resolve';
-    
+    // Creating function to resolve ticket when remove button is clicked
     removeButton.addEventListener('click', function(event) {
         event.stopPropagation();
         ticketContainer.removeChild(customerCard)
     });
    
-    // Add button and ticket 
+    // Add button and ticket to customer card & ticket container 
     customerCard.appendChild(removeButton);
     ticketContainer.appendChild(customerCard);
+
+    // Task 5: Inline Editing 
+
+    // Creat edit button
+    const editButton = document.createElement('button')
+    editButton.textContent = 'Edit';
+    editButton.setAttribute('class', "editButton")  
+    editButton.addEventListener('dblclick', function() {
+    
+        // Pre-populate the input fields with the existing customer name, issue, and priority level.
+        const nameInput = document.createElement('input');
+        nameInput.value = customerName.textContent;
+        
+        const issueInput = document.createElement('input');
+        issueInput.value = customerIssue.textContent;
+
+        const priorityInput = document.createElement('input');
+        priorityInput.value = customerPriority.textContent;
+        
+        // Add the input content to employee card
+        customerCard.appendChild(nameInput);
+        customerCard.appendChild(issueInput);
+        customerCard.appendChild(priorityInput);
+    
+        // Remove static content from employee card
+        customerCard.removeChild(customerName);
+        customerCard.removeChild(customerIssue);
+        customerCard.removeChild(customerPriority);
+    
+        // Create save button when edit button is double clicked
+        const saveButton = document.createElement('button');
+        saveButton.textContent = 'Save';
+        saveButton.setAttribute("class", "saveButton")
+        
+        // Save input when save button is clicked 
+        saveButton.addEventListener('click', function() {
+            // Update text content to input values
+            customerName.textContent = nameInput.value;
+            customerIssue.textContent = issueInput.value;
+            customerPriority.textContent = priorityInput.value;
+            
+            // Add static conent to customer card
+            customerCard.appendChild(customerName);
+            customerCard.appendChild(customerIssue);
+            customerCard.appendChild(customerPriority);
+    
+            // Remove input content from customer card
+            customerCard.removeChild(nameInput);
+            customerCard.removeChild(issueInput);
+            customerCard.removeChild(priorityInput);
+    
+            // Remove save button after saving so it doesn't add everytime edit button is clicked
+            saveButton.remove();
+        })
+        
+        // Add save button when edit button is double clicked
+        customerCard.appendChild(saveButton)
+    })
+    // Add edit button to customer card
+    customerCard.appendChild(editButton)
 }
-
-
 
 // Test Case
 createSupportTicket('Pikachu', 'Cannot Edit Password', 'High');
